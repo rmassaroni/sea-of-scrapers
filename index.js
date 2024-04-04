74337ddf1dfb99930b067fd95936b553db7798d0
rjm <robjmass@gmail.com>
2024-04-04 14:13:26 -0400

    working chromium

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const playwright = require('playwright');
 
 async function main() {
+    
+    const browser = await playwright.chromium.launch({
+        headless: false
+    });
+
+    const page = await browser.newPage();
+
+    await page.waitForTimeout(1000);
+    await browser.close();
+
     console.log("test");
 }
 
